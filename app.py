db2ef49d1d93686ae7a9a51b3378d9529f79e08c
Gustavo Silva <gustavo.silva@blip.pt>
2019-10-23 16:46:33 +0100

    sec: removes exposed secret

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-token = "{flag}git_challenge_surpass"
-
-
 @app.route("/")
 def index():
     return render_template('index.html')
